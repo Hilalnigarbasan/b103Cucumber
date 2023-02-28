c795a9236e3bbe128cd939d54106eb650a8f8cdc
Hilalnigarbasan <hilalnigarbasan@outlook.com>
2023-02-28 16:50:02 +0300

    28.02.23

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -6,15 +6,21 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "./src/test/resources/features", //features folder path --> PATH OF FEATURES
-        glue = "stepdefinitions", //stepdefinitions path  --> PATH OF STEP DEFINITIONS
-        tags = "@tesla or @iphone",
+        plugin = {
+                "pretty",//raporlarin daha ikunakli olmasi icin
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
+        monochrome = true,//raporlarin consoleda okunakli sekilde cikmasi icin
+        features = "./src/test/resources/features",//features folder path
+        glue = "stepdefinitions",   //stepdefinitions path
+        tags = "@personel_olusturma",
         dryRun = false
-
 
 )
 public class Runner {
 }
 //Bu sinif Test caseleri RUN etmek icin kullanilir
 //Ve konfigurasyonlar icin kullanilir
-
+//Runner class, features file lar ile step defitions i birbirile baglar
